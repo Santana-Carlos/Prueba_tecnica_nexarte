e9fb2f9a49ea98a26b480726fcdc8ca93c2a76fb
Diego Colmenares <diego.colmenares@uao.edu.co>
2021-05-13 21:48:10 -0500

    commit 03

--- reactapp/src/Components/Main/Main.js
+++ reactapp/src/Components/Main/Main.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
-import { Container, Select, MenuItem } from "@material-ui/core";
-import { StyledTextField, StyledInput } from "../StyledComponents";
+import {
+  Container,
+  Select,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Backdrop,
+  CircularProgress,
+} from "@material-ui/core";
+import {
+  StyledTextField,
+  StyledInput,
+  BlackButton,
+  GreenButton,
+  StyledCheckbox,
+} from "../StyledComponents";
 import background from "../../Assets/logo.png";
 import "./Main.css";
 
@@ -15,7 +31,14 @@ class Main extends Component {
       temp_numero: "",
       temp_ciudad: "",
       temp_depart: "",
+      set_terms: false,
       api_modelo: [],
+      api_depart: [],
+      api_ciudad: [],
+      show_diag: false,
+      diag_title: "",
+      diag_message: "",
+      loading: true,
     };
   }
 
@@ -32,7 +55,100 @@ class Main extends Component {
       .catch((error) => {
         console.log(error);
       });
+    fetch(process.env.REACT_APP_API_URL + "Departamento", {
+      method: "GET",
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success)
+          this.setState({ loading: false, api_depart: data.departamentos });
+      })
+      .catch((error) => {
+        this.setState({ loading: false });
+        console.log(error);
+      });
   }
+
+  callApiCiudad = () => {
+    fetch(
+      process.env.REACT_APP_API_URL +
+        "CiudadByDepartamento/" +
+        this.state.temp_depart,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.success) this.setState({ api_ciudad: data.ciudades });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  callApiPost = () => {
+    this.setState({
+      loading: true,
+    });
+    const data = {
+      modelo: this.state.temp_modelo,
+      precio: this.state.temp_precio,
+      nombre_cotizante: this.state.temp_nombre,
+      email_cotizante: this.state.temp_email,
+      numero_cotizante: this.state.temp_numero,
+      departamento_id: this.state.temp_depart,
+      ciudad_id: this.state.temp_ciudad,
+    };
+
+    fetch(process.env.REACT_APP_API_URL + "Cotizacion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        if (data.success)
+          this.setState(
+            {
+              loading: false,
+              show_diag: true,
+              diag_title: "Cotización realizada",
+              diag_message:
+                "Los datos de la cotización se han enviado satisfactoriamente",
+            },
+            this.clearTemp
+          );
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ loading: false });
+      });
+  };
+
+  clearTemp = () => {
+    this.setState({
+      temp_modelo: "",
+      temp_precio: "",
+      temp_nombre: "",
+      temp_email: "",
+      temp_numero: "",
+      temp_ciudad: "",
+      temp_depart: "",
+      set_terms: false,
+      api_ciudad: [],
+    });
+  };
 
   handleChange = (e) => {
     const name = e.target.name;
@@ -41,7 +157,13 @@ class Main extends Component {
 
     switch (name) {
       case "input_modelo":
-        this.setState({ temp_modelo: value });
+        this.setState({
+          temp_modelo: value,
+          temp_precio:
+            this.state.api_modelo[
+              this.state.api_modelo.findIndex((x) => x.subtitle === value)
+            ].price,
+        });
         break;
       case "input_nombre":
         this.setState({ temp_nombre: value });
@@ -51,6 +173,15 @@ class Main extends Component {
         break;
       case "input_numero":
         if (value.length <= 10) this.setState({ temp_numero: value });
+        break;
+      case "input_depart":
+        this.setState({ temp_depart: value }, this.callApiCiudad);
+        break;
+      case "input_ciudad":
+        this.setState({ temp_ciudad: value });
+        break;
+      case "input_terms":
+        this.setState({ set_terms: check });
         break;
       default:
         break;
@@ -63,10 +194,11 @@ class Main extends Component {
         <div style={{ padding: "0 2rem 0 1.7rem" }}>
           <img src={background} alt="logo_ssangyong" className="o-logo" />
         </div>
+
         <div className="o-form-container">
           <h1>{"¡Cotiza la tuya!"}</h1>
 
-          <div className="o-label">{"Modelo"}</div>
+          <div className="o-label">{"Selecciona un modelo"}</div>
           <Select
             variant="outlined"
             name="input_modelo"
@@ -122,7 +254,110 @@ class Main extends Component {
             onChange={this.handleChange}
             margin="dense"
           />
+
+          <div className="o-dobleinput-container">
+            <div className="o-dobleinput">
+              <div className="o-label">{"Departamento"}</div>
+              <Select
+                variant="outlined"
+                name="input_depart"
+                value={this.state.temp_depart}
+                onChange={this.handleChange}
+                input={<StyledInput />}
+                margin="dense"
+              >
+                {this.state.api_depart.map((x, i) => (
+                  <MenuItem key={i} value={x.id}>
+                    {x.nombre}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
+            <div className="o-dobleinput">
+              <div className="o-label">{"Ciudad"}</div>
+              <Select
+                variant="outlined"
+                name="input_ciudad"
+                value={this.state.temp_ciudad}
+                onChange={this.handleChange}
+                input={<StyledInput />}
+                margin="dense"
+              >
+                {this.state.api_ciudad[0] === undefined ? (
+                  <MenuItem disabled={true} value={""}>
+                    {"Selecciona un departamento"}
+                  </MenuItem>
+                ) : (
+                  this.state.api_ciudad.map((x, i) => (
+                    <MenuItem key={i} value={x.id}>
+                      {x.nombre}
+                    </MenuItem>
+                  ))
+                )}
+              </Select>
+            </div>
+          </div>
+          <div className="o-terms">
+            <StyledCheckbox
+              checked={this.state.set_terms || false}
+              color="primary"
+              name="input_terms"
+              onChange={this.handleChange}
+            />
+            {"Acepto la Politica de"}
+            <button
+              className="o-link"
+              onClick={() =>
+                this.setState({
+                  show_diag: true,
+                  diag_title: "Política de Tratamiento de Datos",
+                  diag_message: "Politica de tratamiento",
+                })
+              }
+            >
+              {"Tratamiento de Datos"}
+            </button>
+          </div>
+
+          <div className="o-button-post">
+            <BlackButton onClick={this.callApiPost}>
+              {"Enviar datos"}
+            </BlackButton>
+          </div>
         </div>
+
+        <Dialog
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={this.state.show_diag}
+          maxWidth={false}
+        >
+          <DialogTitle style={{ textAlign: "center" }}>
+            {this.state.diag_title}
+          </DialogTitle>
+          <DialogContent>{this.state.diag_message}</DialogContent>
+          <DialogActions style={{ justifyContent: "center" }}>
+            <div className="o-button-diag">
+              <GreenButton
+                onClick={() =>
+                  this.setState({
+                    show_diag: false,
+                  })
+                }
+              >
+                {"Aceptar"}
+              </GreenButton>
+            </div>
+          </DialogActions>
+        </Dialog>
+
+        <Backdrop
+          open={this.state.loading}
+          onClick={() => this.setState({ loading: false })}
+          style={{ zIndex: 2, color: "#fff" }}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
       </Container>
     );
   }
--- reactapp/src/Components/StyledComponents/index.js
+++ reactapp/src/Components/StyledComponents/index.js
@@ -1,40 +1,54 @@
 import { withStyles } from "@material-ui/core/styles";
-import {
-  Container,
-  Button,
-  TextField,
-  Checkbox,
-  Select,
-  OutlinedInput,
-} from "@material-ui/core";
+import { Button, TextField, Checkbox, OutlinedInput } from "@material-ui/core";
 
-export const BlackButton = withStyles((theme) => ({
+export const BlackButton = withStyles(() => ({
   root: {
     display: "flex",
     width: "100%",
     height: "100%",
-    color: "#FFFFFF",
-    fontSize: "1.1rem",
+    color: "#fff",
+    fontSize: "1rem",
+    textTransform: "none",
     margin: 0,
     fontWeight: 400,
     textAlign: "center",
     backgroundColor: "#000",
+    borderRadius: "25px",
     "&:hover": {
       backgroundColor: "#222",
     },
   },
 }))(Button);
 
-export const StyledCheckbox = withStyles((theme) => ({
+export const GreenButton = withStyles(() => ({
+  root: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    color: "#fff",
+    fontSize: "1rem",
+    textTransform: "none",
+    margin: 0,
+    fontWeight: 400,
+    textAlign: "center",
+    backgroundColor: "#47b14c",
+    borderRadius: "25px",
+    "&:hover": {
+      backgroundColor: "#36973a",
+    },
+  },
+}))(Button);
+
+export const StyledCheckbox = withStyles(() => ({
   colorPrimary: {
-    padding: "0 0 0 0.5rem",
+    padding: "0 0.5rem 0 0",
     "&.Mui-checked": {
-      color: "#ff5f58",
+      color: "#000",
     },
   },
 }))(Checkbox);
 
-export const StyledTextField = withStyles((theme) => ({
+export const StyledTextField = withStyles(() => ({
   root: {
     width: "auto",
     margin: "0.2rem 0 0.6rem",
@@ -49,9 +63,9 @@ export const StyledTextField = withStyles((theme) => ({
   },
 }))(TextField);
 
-export const StyledInput = withStyles((theme) => ({
+export const StyledInput = withStyles(() => ({
   root: {
-    width: "auto",
+    width: "100%",
     borderRadius: "25px",
     margin: "0.2rem 0 0.6rem",
     overflow: "hidden",
@@ -67,24 +81,3 @@ export const StyledInput = withStyles((theme) => ({
   focused: {},
   notchedOutline: {},
 }))(OutlinedInput);
-
-export const StyledSelect = withStyles((theme) => ({
-  outlined: {
-    width: "auto",
-    margin: "0.2rem 0 0.6rem",
-    "& .MuiOutlinedInput-root": {
-      borderWidth: "1px",
-      borderRadius: "25px",
-      "&.Mui-focused fieldset": {
-        borderWidth: "2px",
-        borderColor: "#000",
-      },
-    },
-  },
-}))(Select);
-
-export const StyledContainer = withStyles((theme) => ({
-  maxWidthLg: {
-    maxWidth: "90rem",
-  },
-}))(Container);
